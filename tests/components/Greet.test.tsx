68832a29bf6db7f3ee75ba0f22e140448846fc72
Ayan <ayanhasnain2572006@gmail.com>
2025-04-21 04:18:40 +0530

    test

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,10 +1,12 @@
+import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import Greet from "../../src/components/Greet";
-
 describe("Greet", () => {
   it("should render Hello with the name when name is provided", () => {
     render(<Greet name="Ayan Hasnain" />);
-    screen.debug();
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Ayan Hasnain/i);
   });
 });
